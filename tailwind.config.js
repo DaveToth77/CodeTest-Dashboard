3e424d0189247a701440c55aca8553c8a58aefd9
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2022-01-11 14:22:40 -0500

    added: nav bar
    fixed color bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
     extend: {
       colors: {
         'lightBlue': '#A7DCF5',
-        'darkBlue': '##3383A8',
-        'blue': '##46B3E5',
+        'darkBlue': '#3383A8',
+        'blue': '#46B3E5',
         'red': '#E54E6B',
         'off-white':'#F5F9FA'
       },
